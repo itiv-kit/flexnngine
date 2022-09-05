2da86323c8a576d6eb7dd2d5b83810a059210ffe
uzedl <uzedl@student.kit.edu>
2022-09-05 10:08:22 +0200

    [FEAT] read file with more channels

--- hdl/utilities.vhd
+++ hdl/utilities.vhd
@@ -10,7 +10,8 @@ package utilities is
     type array_row_col_t is array (natural  range <>, natural range <>) of std_logic_vector;
 
     type int_line_t is array(natural range <>) of integer;
-    type int_image_t is array (natural  range <>, natural range <>) of integer;
+    type int_image_t is array (natural range <>, natural range <>) of integer;
+    type int_image3_t is array (natural range <>, natural range <>, natural range <>) of integer;
 
     type command_lb_t is (c_lb_idle, c_lb_read, c_lb_read_update, c_lb_shrink);
     type command_lb_array_t is array (natural range <>) of command_lb_t;
@@ -21,6 +22,8 @@ package utilities is
     type command_pe_row_col_t is array (natural range <>, natural range <>) of command_pe_t;
 
     function read_file (file_name : string; num_col : integer; num_row : integer) return int_image_t;
+
+    function read_file (file_name : string; num_col : integer; num_row : integer; num_channels : integer) return int_image3_t;
 
 end package utilities;
 
@@ -55,6 +58,51 @@ package body utilities is
                 v_input_image(v_data_row_counter - 1, i - 1) := v_data_read(i);
 
             end loop;
+
+        end loop;
+
+        return v_input_image;
+
+    end function;
+
+    function read_file (
+        file_name : string;
+        num_col : integer;
+        num_row : integer;
+        num_channels : integer)
+        return int_image3_t is
+
+        file     testfile               : text open read_mode is file_name;
+        variable row                    : line;
+        variable v_data_read            : int_line_t(1 to num_col);
+        variable v_data_row_counter     : integer;
+        variable v_data_channel_counter : integer;
+        variable v_input_image          : int_image3_t(0 to num_channels - 1, 0 to num_row - 1, 0 to num_col - 1);
+
+    begin
+
+        v_data_row_counter     := 0;
+        v_data_channel_counter := 0;
+
+        -- read row from input file
+        while not endfile(testfile) loop
+
+            if v_data_row_counter = num_row then
+                v_data_row_counter     := 0;
+                v_data_channel_counter := v_data_channel_counter + 1;
+            end if;
+
+            readline(testfile, row);
+
+            -- read integer from row
+            for i in 1 to num_col loop
+
+                read(row, v_data_read(i));
+                v_input_image(v_data_channel_counter, v_data_row_counter, i - 1) := v_data_read(i);
+
+            end loop;
+
+            v_data_row_counter := v_data_row_counter + 1;
 
         end loop;
 
