9b4c63c0374c629d1e2ee762ab83972f3f418524
Fabian Lesniak <lesniak@kit.edu>
2023-09-28 20:10:22 +0200

    control: use 10-bit values for all configurations

--- hdl/control.vhd
+++ hdl/control.vhd
@@ -731,8 +731,8 @@ architecture alternative_rs_dataflow of control is
     signal r_count_c0w0 : integer range 0 to 2048; -- range 0 to 511
     signal r_count_c1   : integer range 0 to 1023;
     signal r_count_w1   : integer range 0 to 1023;
-    signal r_count_h2   : integer range 0 to 128;
-    signal r_count_h1   : integer range 0 to 128;
+    signal r_count_h2   : integer range 0 to 1023;
+    signal r_count_h1   : integer range 0 to 1023;
 
     signal w_c0w0         : integer range 0 to 1023;
     signal w_c0w0_last_c1 : integer range 0 to 1023;
@@ -740,8 +740,8 @@ architecture alternative_rs_dataflow of control is
     signal w_c0         : integer range 0 to 1023;
     signal w_c0_last_c1 : integer range 0 to 1023;
 
-    signal w_h2           : integer range 0 to 128;
-    signal w_rows_last_h2 : integer range 0 to 128;
+    signal w_h2           : integer range 0 to 1023;
+    signal w_rows_last_h2 : integer range 0 to 1023;
 
     signal w_w1 : integer range 0 to 1023;
     signal w_c1 : integer range 0 to 1023;
