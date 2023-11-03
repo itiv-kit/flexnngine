86290a0b2ffb453e0ff0b56d19f632c37eae544c
Fabian Lesniak <fabian.lesniak@kit.edu>
2023-11-03 14:03:10 +0100

    ram_dp: use split ports for port a/b to enable bram inference

--- hdl/ram_dp.vhd
+++ hdl/ram_dp.vhd
@@ -22,18 +22,15 @@ entity ram_dp is
         init_file      : string    := ""
     );
     port (
-        clk : in    std_logic; --! Clock input
-
-        wena : in    std_logic;
-        --! Write enable for BRAM port A. If set to '1', the value on #dina will be written to position #addra in the RAM.
-        wenb : in    std_logic;
-        --! Write enable for BRAM port B. If set to '1', the value on #dinb will be written to position #addrb in the RAM.
-        addra : in    std_logic_vector(addr_width - 1 downto 0); --! Address input for reading/writing through port A.
-        addrb : in    std_logic_vector(addr_width - 1 downto 0); --! Address input for reading/writing through port B.
-        dina  : in    std_logic_vector(data_width - 1 downto 0); --! Data to write through port A.
-        dinb  : in    std_logic_vector(data_width - 1 downto 0); --! Data to write through port A.
-        douta : out   std_logic_vector(data_width - 1 downto 0); --! Outputs the data in the BRAM at #addra
-        doutb : out   std_logic_vector(data_width - 1 downto 0)  --! Outputs the data in the BRAM at #addrb
+        clk   : in  std_logic; --! Clock input
+        wena  : in  std_logic; --! Write enable for BRAM port A. If set to '1', the value on #dina will be written to position #addra in the RAM.
+        wenb  : in  std_logic; --! Write enable for BRAM port B. If set to '1', the value on #dinb will be written to position #addrb in the RAM.
+        addra : in  std_logic_vector(addr_width - 1 downto 0); --! Address input for reading/writing through port A.
+        addrb : in  std_logic_vector(addr_width - 1 downto 0); --! Address input for reading/writing through port B.
+        dina  : in  std_logic_vector(data_width - 1 downto 0); --! Data to write through port A.
+        dinb  : in  std_logic_vector(data_width - 1 downto 0); --! Data to write through port A.
+        douta : out std_logic_vector(data_width - 1 downto 0); --! Outputs the data in the BRAM at #addra
+        doutb : out std_logic_vector(data_width - 1 downto 0)  --! Outputs the data in the BRAM at #addrb
     );
 end entity ram_dp;
 
@@ -45,16 +42,12 @@ architecture syn of ram_dp is
     signal doutb_s : std_logic_vector(data_width - 1 downto 0);
 
     impure function init_memory_wfile (mem_file_name : in string) return ram_type is
-
         file     mem_file : text open read_mode is mem_file_name;
         variable mem_line : line;
         variable temp_bv  : bit_vector(data_width - 1 downto 0);
         variable temp_mem : ram_type;
-
     begin
-
         for i in ram_type'range loop
-
             if not endfile(mem_file) then
                 readline(mem_file, mem_line);
                 read(mem_line, temp_bv);
@@ -62,48 +55,46 @@ architecture syn of ram_dp is
             else
                 temp_mem(i) := (others => '0');
             end if;
-
         end loop;
-
         return temp_mem;
-
     end function;
 
     impure function init_file_or_zero (mem_file_name : in string) return ram_type is
     begin
-
         if initialize then
             return init_memory_wfile(mem_file_name);
         else
             return (others => (others => '0'));
         end if;
-
     end function;
 
-    signal ram_instance : ram_type := init_file_or_zero(init_file);
+    shared variable ram_instance : ram_type := init_file_or_zero(init_file);
 
 begin
 
-    ports : process (clk) is
+    port_a : process is
     begin
-
-        if rising_edge(clk) then
-            if wena = '1' then
-                ram_instance(to_integer(unsigned(addra))) <= dina;
-                douta_s                                   <= dina;
-            else
-                douta_s <= ram_instance(to_integer(unsigned(addra)));
-            end if;
-
-            if wenb = '1' then
-                ram_instance(to_integer(unsigned(addrb))) <= dinb;
-                doutb_s                                   <= dinb;
-            else
-                doutb_s <= ram_instance(to_integer(unsigned(addrb)));
-            end if;
+        wait until rising_edge(clk);
+        if wena = '1' then
+            ram_instance(to_integer(unsigned(addra))) := dina;
+            douta_s                                   <= dina;
+        else
+            douta_s <= ram_instance(to_integer(unsigned(addra)));
         end if;
 
-    end process ports;
+    end process port_a;
+
+    port_b : process is
+    begin
+        wait until rising_edge(clk);
+        if wenb = '1' then
+            ram_instance(to_integer(unsigned(addrb))) := dinb;
+            doutb_s                                   <= dinb;
+        else
+            doutb_s <= ram_instance(to_integer(unsigned(addrb)));
+        end if;
+
+    end process port_b;
 
     g0_use_output_reg_0 : if use_output_reg = '0' generate   -- directly connected with the output
         douta <= douta_s;
@@ -112,16 +103,12 @@ begin
 
     g0_use_output_reg_1 : if use_output_reg = '1' generate   -- optional output register is being used
 
-        output_buffer : process (clk) is
+        output_buffer : process is
         begin
-
-            if rising_edge(clk) then
-                douta <= douta_s;
-                doutb <= doutb_s;
-            end if;
-
+            wait until rising_edge(clk);
+            douta <= douta_s;
+            doutb <= doutb_s;
         end process output_buffer;
-
     end generate g0_use_output_reg_1;
 
 end architecture syn;
