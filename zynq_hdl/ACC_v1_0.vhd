ea7725ca14c409e6acf83a4d5cbe4bef7a60d6d9
Fabian Lesniak <fabian.lesniak@kit.edu>
2024-05-17 18:39:16 +0200

    axi top: switch to entity instantiation

--- zynq_hdl/ACC_v1_0.vhd
+++ zynq_hdl/ACC_v1_0.vhd
@@ -107,131 +107,6 @@ entity ACC_v1_0 is
 end ACC_v1_0;
 
 architecture arch_imp of ACC_v1_0 is
-  component ACC_v1_0_S00_AXI is
-    generic (
-      NUM_REGS : integer := 32;
-      size_x : positive := 5;
-      size_y : positive := 5;
-      line_length_iact : positive := 512;
-      line_length_psum : positive := 1024;
-      line_length_wght : positive := 512;
-      data_width_iact : positive := 8;
-      data_width_wght : positive := 8;
-      data_width_psum : positive := 16;
-      spad_axi_addr_width_iact : positive := 16;
-      spad_axi_addr_width_wght : positive := 16;
-      spad_axi_addr_width_psum : positive := 17;
-      C_S_AXI_DATA_WIDTH : integer := 32;
-      C_S_AXI_ADDR_WIDTH : integer := 7
-    );
-    port (
-      o_rst         : out   std_logic;
-      o_start       : out   std_logic;
-      i_done        : in    std_logic;
-      o_params      : out   parameters_t;
-      S_AXI_ACLK    : in std_logic;
-      S_AXI_ARESETN : in std_logic;
-      S_AXI_AWADDR  : in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
-      S_AXI_AWPROT  : in std_logic_vector(2 downto 0);
-      S_AXI_AWVALID : in std_logic;
-      S_AXI_AWREADY : out std_logic;
-      S_AXI_WDATA   : in std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
-      S_AXI_WSTRB   : in std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
-      S_AXI_WVALID  : in std_logic;
-      S_AXI_WREADY  : out std_logic;
-      S_AXI_BRESP   : out std_logic_vector(1 downto 0);
-      S_AXI_BVALID  : out std_logic;
-      S_AXI_BREADY  : in std_logic;
-      S_AXI_ARADDR  : in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
-      S_AXI_ARPROT  : in std_logic_vector(2 downto 0);
-      S_AXI_ARVALID : in std_logic;
-      S_AXI_ARREADY : out std_logic;
-      S_AXI_RDATA   : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
-      S_AXI_RRESP   : out std_logic_vector(1 downto 0);
-      S_AXI_RVALID  : out std_logic;
-      S_AXI_RREADY  : in std_logic
-    );
-  end component ACC_v1_0_S00_AXI;
-
-  component accelerator is
-    generic (
-      size_x    : positive := 5;
-      size_y    : positive := 5;
-      size_rows : positive := 9;
-
-      addr_width_rows : positive := 4;
-      addr_width_y    : positive := 3;
-      addr_width_x    : positive := 3;
-
-      -- iact word size, pe line buffer length & matching offset addressing width
-      data_width_iact     : positive := 8;
-      line_length_iact    : positive := 512;
-      addr_width_iact     : positive := 9;
-
-      -- psum word size, pe line buffer length & matching offset addressing width
-      data_width_psum     : positive := 16;
-      line_length_psum    : positive := 1024;
-      addr_width_psum     : positive := 10;
-
-      -- wght word size, pe line buffer length & matching offset addressing width
-      data_width_wght     : positive := 8;
-      line_length_wght    : positive := 512;
-      addr_width_wght     : positive := 9;
-
-      -- address widths scratchpad <-> pe array
-      spad_addr_width_iact : positive := 15; -- word size data_width_iact
-      spad_addr_width_wght : positive := 15; -- word size data_width_wght
-      spad_addr_width_psum : positive := 15; -- word size data_width_psum
-
-      -- address widths scratchpad <-> external, port_a is exposed as i/o on this module
-      spad_ext_addr_width_iact : positive := 13; -- word size spad_ext_data_width_iact
-      spad_ext_addr_width_wght : positive := 13; -- word size spad_ext_data_width_wght
-      spad_ext_addr_width_psum : positive := 14; -- word size spad_ext_data_width_psum
-      spad_ext_data_width_iact : positive := 32;
-      spad_ext_data_width_wght : positive := 32;
-      spad_ext_data_width_psum : positive := 32;
-
-      fifo_width : positive := 16;
-
-      g_iact_fifo_size : positive := 15;
-      g_wght_fifo_size : positive := 15;
-      g_psum_fifo_size : positive := 45;
-
-      g_files_dir : string  := "";
-      g_init_sp   : boolean := false;
-      g_dataflow  : integer := 1
-    );
-    port (
-      clk  : in    std_logic;
-      rstn : in    std_logic;
-
-      clk_sp : in    std_logic;
-
-      i_start  : in    std_logic;
-      o_done   : out   std_logic;
-      i_params : in    parameters_t;
-
-      i_en_iact : in    std_logic;
-      i_en_wght : in    std_logic;
-      i_en_psum : in    std_logic;
-
-      i_write_en_iact : in    std_logic_vector(spad_ext_data_width_iact/8 - 1 downto 0);
-      i_write_en_wght : in    std_logic_vector(spad_ext_data_width_wght/8 - 1 downto 0);
-      i_write_en_psum : in    std_logic_vector(spad_ext_data_width_psum/8 - 1 downto 0);
-
-      i_addr_iact : in    std_logic_vector(spad_ext_addr_width_iact - 1 downto 0);
-      i_addr_wght : in    std_logic_vector(spad_ext_addr_width_wght - 1 downto 0);
-      i_addr_psum : in    std_logic_vector(spad_ext_addr_width_psum - 1 downto 0);
-
-      i_din_iact : in    std_logic_vector(spad_ext_data_width_iact - 1 downto 0);
-      i_din_wght : in    std_logic_vector(spad_ext_data_width_wght - 1 downto 0);
-      i_din_psum : in    std_logic_vector(spad_ext_data_width_psum - 1 downto 0);
-
-      o_dout_iact : out   std_logic_vector(spad_ext_data_width_iact - 1 downto 0);
-      o_dout_wght : out   std_logic_vector(spad_ext_data_width_wght - 1 downto 0);
-      o_dout_psum : out   std_logic_vector(spad_ext_data_width_psum - 1 downto 0)
-    );
-  end component;
 
   signal rst, rstn : std_logic;
   signal start     : std_logic;
@@ -278,7 +153,7 @@ begin
   rstn <= not rst;
 
   -- Instantiation of AXI Bus Interface S00_AXI
-  ACC_v1_0_S00_AXI_inst : ACC_v1_0_S00_AXI generic map (
+  ACC_v1_0_S00_AXI_inst : entity xil_defaultlib.ACC_v1_0_S00_AXI generic map (
     C_S_AXI_DATA_WIDTH => C_S00_AXI_DATA_WIDTH,
     C_S_AXI_ADDR_WIDTH => C_S00_AXI_ADDR_WIDTH,
     size_x => size_x,
@@ -320,7 +195,7 @@ begin
     S_AXI_RREADY  => s00_axi_rready
   );
 
-  accelerator_inst : accelerator generic map (
+  accelerator_inst : entity accel.accelerator generic map (
     size_x    => size_x,
     size_y    => size_y,
     size_rows => size_x + size_y - 1,
