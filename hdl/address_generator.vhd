5eb688657669c498d789513f228a37cf78000f6a
Fabian Lesniak <fabian.lesniak@kit.edu>
2024-08-15 10:44:30 +0200

    code cleanup for address_generator.vhd

--- hdl/address_generator.vhd
+++ hdl/address_generator.vhd
@@ -57,9 +57,6 @@ architecture rs_dataflow of address_generator is
     signal w_c0_iact       : integer;
     signal r_count_c0_iact : integer;
 
-    -- signal r_W0 : integer; Kernel size
-    -- signal r_count_w0 : integer; Kernel size
-
     signal w_w1            : integer;
     signal r_count_w1_iact : integer;
 
@@ -91,9 +88,6 @@ architecture rs_dataflow of address_generator is
     signal w_c0_wght       : integer;
     signal r_count_c0_wght : integer;
 
-    -- signal r_W0 : integer; Kernel size
-    -- signal r_count_w0 : integer; Kernel size
-
     signal r_count_w1_wght : integer;
 
     signal r_count_c1_wght : integer;
@@ -117,9 +111,6 @@ architecture rs_dataflow of address_generator is
 
     signal r_delay_wght_valid : std_logic_vector(size_y - 1 downto 0);
 
-    signal r_test_wght  : int_line_t(0 to size_y - 1);
-    signal r_test_wght2 : int_line_t(0 to size_y - 1);
-
 begin
 
     w_c1      <= i_params.c1;
@@ -140,9 +131,9 @@ begin
 
     iact_address_out : for i in 0 to size_rows - 1 generate
 
-        /*o_address_iact(i)     <= std_logic_vector(to_unsigned(w_offset_mem_iact + i * i_params.image_x, addr_width_iact_mem));
-        o_address_iact_valid(i) <= '1' when i_start = '1' and i_fifo_full_iact = '0' and r_iact_done = '0' else --
-                                   '0';*/
+        -- o_address_iact(i)       <= std_logic_vector(to_unsigned(w_offset_mem_iact + i * i_params.image_x, addr_width_iact_mem));
+        -- o_address_iact_valid(i) <= '1' when i_start = '1' and i_fifo_full_iact = '0' and r_iact_done = '0' else
+        --                            '0';
 
         iact_address_out : process (clk, rstn) is
         begin
@@ -174,9 +165,9 @@ begin
 
     wght_address_out : for i in 0 to size_y - 1 generate
 
-        /*o_address_wght(i)       <= std_logic_vector(to_unsigned(w_offset_mem_wght + i * i_params.kernel_size, addr_width_iact_mem));
-        o_address_wght_valid(i) <= '1' when i_start = '1' and i_fifo_full_wght = '0' and r_wght_done = '0' else --
-                                   '0';*/
+        -- o_address_wght(i)       <= std_logic_vector(to_unsigned(w_offset_mem_wght + i * i_params.kernel_size, addr_width_iact_mem));
+        -- o_address_wght_valid(i) <= '1' when i_start = '1' and i_fifo_full_wght = '0' and r_wght_done = '0' else
+        --                            '0';
 
         wght_address_out : process (clk, rstn) is
         begin
@@ -185,8 +176,6 @@ begin
                 o_address_wght(i)       <= (others => '0');
                 o_address_wght_valid(i) <= '0';
                 r_delay_wght_valid(i)   <= '0';
-                r_test_wght(i)          <= 0;
-                r_test_wght2(i)         <= 0;
             elsif rising_edge(clk) then
                 r_delay_wght_valid(i) <= '0';
                 if i_start = '1' and i_fifo_full_wght = '0' and r_wght_done = '0' and r_delay_wght_valid(i) = '0' then
@@ -196,8 +185,6 @@ begin
                     if i < r_mapped_pe_rows then
                         o_address_wght(i) <= std_logic_vector(to_unsigned(w_offset_mem_wght + (i - (to_integer(unsigned(i_m0_dist(i)))) * i_params.kernel_size + i_params.kernel_size) * i_params.kernel_size +
                                                                           ((to_integer(unsigned(i_m0_dist(i))) - 1) * i_params.kernel_size * i_params.kernel_size * i_params.inputchs), addr_width_wght_mem));
-                        r_test_wght(i)    <= (i - (to_integer(unsigned(i_m0_dist(i)))) * i_params.kernel_size + i_params.kernel_size);
-                        r_test_wght2(i)   <= ((to_integer(unsigned(i_m0_dist(i))) - 1) * i_params.kernel_size * i_params.kernel_size * i_params.inputchs);
                     end if;
                 else
                     o_address_wght_valid(i) <= '0';
@@ -397,36 +384,6 @@ begin
 
     end process p_wght_counter;
 
-    /*p_calc_psum_offsets : process (clk, rstn) is
-    begin
-
-        if not rstn then
-            r_address_offsets_psum      <= (others => (others => '0'));
-            r_address_offsets_psum_done <= '0';
-            r_address_offsets_count_x   <= 0;
-        elsif rising_edge(clk) then
-            if i_start = '1' and not r_address_offsets_psum_done = '1' then
-                if r_address_offsets_count_x /= size_x - 1 then
-                    r_address_offsets_psum(r_address_offsets_count_x + 1) <= std_logic_vector(to_unsigned(to_integer(unsigned(r_address_offsets_psum(r_address_offsets_count_x)) + i_params.w1), addr_width_psum_mem));
-                    r_address_offsets_count_x                             <= r_address_offsets_count_x + 1;
-                else
-                    r_address_offsets_psum_done <= '1';
-                end if;
-            end if;
-        end if;
-
-    end process p_calc_psum_offsets;
-
-    p_psum_counter : process (clk, rstn) is
-    begin
-
-        if not rstn then
-        elsif rising_edge(clk) then
-        -- tiles_x
-        end if;
-
-    end process p_psum_counter;*/
-
 end architecture rs_dataflow;
 
 architecture alternative_rs_dataflow of address_generator is
@@ -439,9 +396,6 @@ architecture alternative_rs_dataflow of address_generator is
 
     signal w_c0_iact       : integer;
     signal r_count_c0_iact : integer;
-
-    -- signal r_W0 : integer; Kernel size
-    -- signal r_count_w0 : integer; Kernel size
 
     signal w_w1            : integer;
     signal r_count_w1_iact : integer range 0 to 512;
@@ -476,9 +430,6 @@ architecture alternative_rs_dataflow of address_generator is
     signal w_c0_wght       : integer;
     signal r_count_c0_wght : integer;
 
-    -- signal r_W0 : integer; Kernel size
-    -- signal r_count_w0 : integer; Kernel size
-
     signal r_count_w1_wght : integer range 0 to 512;
 
     signal r_count_c1_wght : integer range 0 to 512;
@@ -503,9 +454,6 @@ architecture alternative_rs_dataflow of address_generator is
 
     signal r_delay_wght_valid : std_logic_vector(size_y - 1 downto 0);
 
-    signal r_test_wght  : int_line_t(0 to size_y - 1);
-    signal r_test_wght2 : int_line_t(0 to size_y - 1);
-
     signal r_ckk  : integer;
     signal r_ckki : int_line_t(0 to size_y - 1);
 
@@ -529,9 +477,9 @@ begin
 
     iact_address_out : for i in 0 to size_rows - 1 generate
 
-        /*o_address_iact(i)     <= std_logic_vector(to_unsigned(w_offset_mem_iact + i * i_params.image_x, addr_width_iact_mem));
-        o_address_iact_valid(i) <= '1' when i_start = '1' and i_fifo_full_iact = '0' and r_iact_done = '0' else --
-                                   '0';*/
+        -- o_address_iact(i)       <= std_logic_vector(to_unsigned(w_offset_mem_iact + i * i_params.image_x, addr_width_iact_mem));
+        -- o_address_iact_valid(i) <= '1' when i_start = '1' and i_fifo_full_iact = '0' and r_iact_done = '0' else --
+        --                            '0';
 
         iact_address_out : process (clk, rstn) is
         begin
@@ -581,8 +529,6 @@ begin
                 o_address_wght(i)       <= (others => '0');
                 o_address_wght_valid(i) <= '0';
                 r_delay_wght_valid(i)   <= '0';
-                r_test_wght(i)          <= 0;
-                r_test_wght2(i)         <= 0;
                 r_ckki(i)               <= 0;
             elsif rising_edge(clk) then
                 r_delay_wght_valid(i) <= '0';
@@ -593,8 +539,6 @@ begin
                     -- o_address_wght(i)       <= std_logic_vector(to_unsigned(w_offset_mem_wght + i * i_params.kernel_size, addr_width_wght_mem));
                     if i < r_mapped_pe_rows then
                         o_address_wght(i) <= std_logic_vector(to_unsigned(w_offset_mem_wght + r_ckki(i) + r_count_h1_wght * i_params.kernel_size, addr_width_wght_mem)); -- channel offset + kernel offset + row offset
-                        /* r_test_wght(i)    <= (i - i_params.kernel_size + i_params.kernel_size);
-                         r_test_wght2(i)   <= (i * i_params.kernel_size * i_params.kernel_size * i_params.inputchs);*/
                     end if;
                 else
                     o_address_wght_valid(i) <= '0';
@@ -820,35 +764,5 @@ begin
         end if;
 
     end process p_wght_counter;
-
-    /*p_calc_psum_offsets : process (clk, rstn) is
-    begin
-
-        if not rstn then
-            r_address_offsets_psum      <= (others => (others => '0'));
-            r_address_offsets_psum_done <= '0';
-            r_address_offsets_count_x   <= 0;
-        elsif rising_edge(clk) then
-            if i_start = '1' and not r_address_offsets_psum_done = '1' then
-                if r_address_offsets_count_x /= size_x - 1 then
-                    r_address_offsets_psum(r_address_offsets_count_x + 1) <= std_logic_vector(to_unsigned(to_integer(unsigned(r_address_offsets_psum(r_address_offsets_count_x)) + i_params.w1), addr_width_psum_mem));
-                    r_address_offsets_count_x                             <= r_address_offsets_count_x + 1;
-                else
-                    r_address_offsets_psum_done <= '1';
-                end if;
-            end if;
-        end if;
-
-    end process p_calc_psum_offsets;
-
-    p_psum_counter : process (clk, rstn) is
-    begin
-
-        if not rstn then
-        elsif rising_edge(clk) then
-        -- tiles_x
-        end if;
-
-    end process p_psum_counter;*/
 
 end architecture alternative_rs_dataflow;
